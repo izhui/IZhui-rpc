0d99cdc8cb60dec61ee23f34e165820ab1e85504
张嘉华 <admin@cavacn.com>
2015-12-22 10:34:53 +0800

    增加readme，workerlist
    增加readme，workerlist

--- example/client.js
+++ example/client.js
@@ -7,7 +7,15 @@ let client = Rpc.createClient( 'Cavacn.com@Client', 'Client客户端');
 client.on('error',function(err){
     console.log('error',err);
 }).on('connect',function(){
-    client.invoke('/demo/aaa',{ uname: 'cavacn', upass: 'cavacnpwd', age: 'hello'}  , console.log);
-    client.workerList(console.log);
+    // client.invoke('/demo/aaa',{ uname: 'cavacn', upass: 'cavacnpwd', age: 'hello'}  , console.log);
+    client.workerList(function( err, result ){
+        // console.log(result);
+        var data = result.result;
+        data.forEach(function(item,index){
+            client.readme(item.id,function(err,readme){
+                console.log(readme.result);
+            });
+        });
+    });
 })
 .connect( 8888 );
--- example/worker2.js
+++ example/worker2.js
@@ -1,7 +1,7 @@
 'use strict';
 const Rpc = require("../");
 
-let worker = Rpc.createWorker( 'demo2', '测试案例2');
+let worker = Rpc.createWorker( 'demo2', '测试案例2','用于测试的第二个案例');
 
 worker.on('/:name',function( request, callback){
     callback( 200, null, request );
@@ -12,9 +12,9 @@ worker.on('/:name',function( request, callback){
 .on('connect',function(){
     // console.log('connect');
     let self = this;
-    setInterval(function(){
+    //setInterval(function(){
         self.remoteInvoke('/demo/hello',{ uname: Date.now(), upass: Date.now(), age: '12' },console.log);
-    },1000);
+    //},1000);
 })
 .listen( 5002 )
 .connect( 8888 ); 
