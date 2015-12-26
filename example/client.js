ac58a0697cff7c4b80a2cde12795f17652ab49ab
张嘉华 <admin@cavacn.com>
2015-12-26 16:22:56 +0800

    增加ASK函数
    增加ASK函数

--- example/client.js
+++ example/client.js
@@ -7,14 +7,17 @@ let client = Rpc.createClient( 'Cavacn.com@Client', 'Client客户端');
 client.on('error',function(err){
     console.log('error',err);
 }).on('connect',function(){
-    client.invoke('/demo/0.2/aaa',{ uname: 'cavacn', upass: 'cavacnpwd', age: '12'}  , console.log);
-    client.workerList(function( status, error, result ){
-        var data = result;
-        data.forEach(function(item,index){
-            client.readme(item.id,function(status,error,readme){
-                console.log(status,error,readme);
-            });
-        });
+    client.ask('/demo/0.2/aaa',function(err,client){
+        console.log( client ? client.serviceId : '连接失败');
     });
+    // client.invoke('/demo/0.2/aaa',{ uname: 'cavacn', upass: 'cavacnpwd', age: '12'}  , console.log);
+    // client.workerList(function( status, error, result ){
+        // var data = result;
+        // data.forEach(function(item,index){
+            // client.readme(item.id,function(status,error,readme){
+                // console.log(status,error,readme);
+            // });
+        // });
+    // });
 })
 .connect( 8888 );
