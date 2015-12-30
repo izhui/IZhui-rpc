96bb472706f8ac62292a586ffee9ca98da4357fd
张嘉华 <admin@cavacn.com>
2015-12-30 14:20:22 +0800

    减轻master的压力

--- example/client.js
+++ example/client.js
@@ -7,9 +7,9 @@ let client = Rpc.createClient( 'Cavacn.com@Client', 'Client客户端');
 client.on('error',function(err){
     console.log('error',err);
 }).on('connect',function(){
-    client.ask('/demo/0.2/aaa',function(err,client){
-        console.log( client ? client.serviceId : '连接失败');
-    });
+    //client.ask('/demo/0.2/aaa',function(err,client){
+    //    console.log( '===>',client ? client.serviceId : 'connect fail');
+    //});
     // client.invoke('/demo/0.2/aaa',{ uname: 'cavacn', upass: 'cavacnpwd', age: '12'}  , console.log);
     // client.workerList(function( status, error, result ){
         // var data = result;
@@ -19,5 +19,8 @@ client.on('error',function(err){
             // });
         // });
     // });
+    //setInterval(function(){
+    //    client.invoke('/demo/0.2/aaa',{ uname: 'cavacn', upass: 'cavacnpwd', age: '12'},console.log);
+    //},1000);
 })
 .connect( 8888 );
